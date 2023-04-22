100f3948d883c0be23faaf53187b2062f1f0929b
jurba1997 <jurba2011@gmail.com>
2023-04-22 17:15:35 +0000

    add multiplication code to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,9 @@ function runGame(gameType){
     if(gameType=== "addition"){
         displayAdditionQuestion(num1, num2);
     }
+    else if(gameType=== "multiply"){
+        displayMultiplyQuestion(num1, num2)
+    }
     else{
         alert(`Unknown game type: ${gameType}`)
         throw(`Uknown gmae type: ${gameType}. Aborting!`)
@@ -71,6 +74,9 @@ function calculateCorrectAnswer(){
     if (operator === "+"){
         return[operand1 + operand2, "addition"];
     }
+    else if(operator ==="x"){
+        return[operand1 * operand2, "multiply"];
+    }
     else{
         alert(`Unimplemented operator ${operator}`);
         throw`Unimplemented operator ${operator}. Aborting!`;
@@ -97,6 +103,7 @@ function incrementWrongAnswer(){
 }
 
 function displayAdditionQuestion(operand1, operand2){
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
@@ -106,11 +113,14 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
 
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion(){
+function displayDivisionQuestion(operand1, operand2){
 
 }
 
