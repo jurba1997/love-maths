5e694fcd063219250453c3d3048fe30b5e04b40e
jurba1997 <jurba2011@gmail.com>
2023-04-23 18:14:14 +0000

    solve the division problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,9 @@ function runGame(gameType){
     else if(gameType=== "subtract"){
         displaySubtractQuestion(num1, num2);
     }
+    else if(gameType === "division"){
+        displayDivisionQuestion(num1, num2);
+    }
     else{
         alert(`Unknown game type: ${gameType}`)
         throw(`Uknown gmae type: ${gameType}. Aborting!`)
@@ -92,6 +95,9 @@ function calculateCorrectAnswer(){
     else if(operator === "-"){
         return[operand1 - operand2, "subtract"];
     }
+    else if(operator === "/"){
+        return[operand1 / operand2, "division"];
+    }
     else{
         alert(`Unimplemented operator ${operator}`);
         throw`Unimplemented operator ${operator}. Aborting!`;
@@ -139,6 +145,10 @@ function displayMultiplyQuestion(operand1, operand2){
 }
 
 function displayDivisionQuestion(operand1, operand2){
-
+    operand1 = operand1 * 10;
+    operand1 = Math.floor(operand1 / operand2) * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
 
