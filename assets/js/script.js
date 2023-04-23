cbed52303aa2d76aed1c3ce78108c53d2b4e9313
jurba1997 <jurba2011@gmail.com>
2023-04-23 16:08:59 +0000

    finish tidying up

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,14 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
-    runGame("addition")
-;})
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter");
+        checkAnswer();
+    })
+
+    runGame("addition");
+})
 
 
 /**
@@ -22,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after user's answwer has been processed
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
